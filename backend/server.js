c8d00a92f8911daeb2121b5ef7598a3ccdfdc5ad
Wilfred <wilfred5777@gmail.com>
2022-02-17 15:21:20 +0800

    12. Seving Products - Back End Routes Fake Data

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const products = require("./data/products");
+
+const app = express();
+
+app.use(express.json());
+
+/// ROUTES
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+app.get("/api/products", (req, res) => {
+  //   res.send("API PRODUCTS");
+  res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
