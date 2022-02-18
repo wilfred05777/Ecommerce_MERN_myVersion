39b2b9fd1b1ed6ec801868b2dd6f037bc58aac7e
Wilfred <wilfred5777@gmail.com>
2022-02-18 15:45:39 +0800

    set server prompt

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,7 @@ app.get("/api/products/:id", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server is running ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
