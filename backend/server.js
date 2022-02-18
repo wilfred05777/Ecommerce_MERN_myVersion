95636d9e556a621fae820c4f37d9461e55dd674c
Wilfred <wilfred5777@gmail.com>
2022-02-18 18:10:55 +0800

    19. Adding Colors To The Console (Optional)

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
 import connectDB from "./config/db.js";
+import colors from "colors";
 
 dotenv.config();
 
@@ -31,5 +32,7 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(`Server is running ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server is running ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
