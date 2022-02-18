607ca25c608708b330282fb5fb5e068a4d540a80
Wilfred <wilfred5777@gmail.com>
2022-02-18 18:47:46 +0800

    21. Preparing Sample Data User example data

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Juan Dela Cruz",
+    email: "juan@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Jose Rizal",
+    email: "jose@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default Users;
