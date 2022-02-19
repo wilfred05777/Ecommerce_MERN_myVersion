ea7c753bae6633a7566fcf9f3ce5c19c071da806
Wilfred <wilfred5777@gmail.com>
2022-02-20 05:49:56 +0800

    29. Bringing Redux State Into HomeScreen - useDispatch & useSelector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
-
-import axios from "axios";
-// import products from "../products";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch(); /// using hooks
 
+  // step2: useSelector to grab the list of Products from the state
+  const productList = useSelector((state) => state.productList);
+
+  // step3: pull out
+  const { loading, error, products } = productList;
+
+  // step1: fire the lists through action
   useEffect(() => {
-    // await axios.get("/api/products/"); /// dili pwede mag butang ug async sa useEffect( async()=>{})
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
+    dispatch(listProducts());
+  }, [dispatch]);
 
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
+  // const products = [];
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            {/* <h3>{product.name}</h3> */}
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading </h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {/* step4: display the output */}
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              {/* <h3>{product.name}</h3> */}
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
