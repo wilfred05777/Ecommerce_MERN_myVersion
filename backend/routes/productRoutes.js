a3685205d8025f89559a9942d50a96a97a75719a
Wilfred <wilfred5777@gmail.com>
2022-02-19 15:05:55 +0800

    trial and error on response message for backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -20,14 +20,23 @@ router.get(
 // @access Public
 router.get(
   "/:id",
-  AsyncHandler(async (req, res) => {
+  AsyncHandler(async (req, res, next) => {
     //   const product = products.find((p) => p._id === req.params.id);
     const product = await Product.findById(req.params.id);
-    if (product) {
+
+    try {
+      if (!product) {
+        res.json("Product not found!");
+      }
       res.json(product);
-    } else {
-      res.status(404).json({ message: "Product not found" });
+    } catch (error) {
+      next(error);
     }
+    // if (product) {
+    //   res.json(product);
+    // } else {
+    //   res.status(404).json({ message: "Product not found" });
+    // }
   })
 );
 
