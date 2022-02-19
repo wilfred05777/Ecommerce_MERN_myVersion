9f433fec070610392106bcfb701ac1d2a741427b
Wilfred <wilfred5777@gmail.com>
2022-02-19 18:21:21 +0800

    trial & error MongoDB 1 to many Relationship

--- backend/models/contactModel.js
+++ backend/models/contactModel.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const contactSchema = mongoose.Schema(
   {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
+    // user: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   required: true,
+    //   ref: "User",
+    // },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     message: { type: String },
