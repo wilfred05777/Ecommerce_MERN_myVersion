b8f6fbab7f1e06dc401effa69962ac0c4dbd5bfe
Wilfred <wilfred5777@gmail.com>
2022-02-19 11:14:29 +0800

    discard this testing for seeder

--- backend/models/product.route.js
+++
@@ -1,9 +0,0 @@
-import Product_controller from "./product.controller";
-
-import express from "express";
-
-const router = express.Router();
-
-router.post("/create", Product_controller.create);
-
-module.exports = router;
