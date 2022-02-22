6b89d2181b3f1c7064d305437dd6805e3465c7b2
Wilfred <wilfred5777@gmail.com>
2022-02-23 04:51:51 +0800

    43. User Registration & Password Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,6 +27,39 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body; /// plain text
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.name,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
+});
+
 // @desc GET user profile
 // @route GET /api/users/profile
 // @access Private
@@ -45,6 +78,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error("User not found");
   }
 });
-// hooking this route
 
-export { authUser, getUserProfile };
+// hooking this route
+export { authUser, getUserProfile, registerUser };
