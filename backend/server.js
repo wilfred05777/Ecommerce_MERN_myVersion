bff61a107d1374564f45971b1ce27875ed460060
Wilfred <wilfred05777@gmail.com>
2022-04-22 05:20:53 +0800

    81. Front End Image Upload

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/orders", orderRoutes);
-app.use("/api/orders", uploadRoutes);
+app.use("/api/upload", uploadRoutes);
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
